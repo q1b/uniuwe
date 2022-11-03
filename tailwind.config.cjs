0e168621071f309da7021d1113d74694b0f35403
sacarvy <sukhpreetben10@gmail.com>
2022-11-03 16:27:53 +0530

    extended aspectRation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,18 @@ module.exports = {
 			xxs: '360px',
 			xs: '480px', // => @media (min-width: 480px) { ... }
 			...defaultTheme.screens,
+    },
+    extend: {
+			aspectRatio: {
+				auto: 'auto',
+				square: '1 / 1',
+				video: '16 / 9',
+				landscape: '4 / 3',
+				portrait: '3 / 4',
+				widescreen: '16 / 9',
+				ultrawide: '18 / 5',
+				golden: '1.6180 / 1',
+			},
 		},
 	},
 	plugins: [require('@tailwindcss/typography')],
