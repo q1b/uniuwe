5755defea603b3da24e65252f50efd69f9a40649
sacarvy <sukhpreetben10@gmail.com>
2022-11-03 16:27:01 +0530

    extended screen breakpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
   darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,mdx,svelte,ts,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+			xxs: '360px',
+			xs: '480px', // => @media (min-width: 480px) { ... }
+			...defaultTheme.screens,
+		},
 	},
 	plugins: [require('@tailwindcss/typography')],
 };
